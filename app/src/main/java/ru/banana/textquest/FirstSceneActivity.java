3c6c017ef016a0efba160896d10a8cdb5ff8efe8
killkill854 <killkill854@gmail.com>
2016-12-21 18:45:30 +0300

    14123
    1241

--- app/src/main/java/ru/banana/textquest/FirstSceneActivity.java
+++ app/src/main/java/ru/banana/textquest/FirstSceneActivity.java
@@ -199,4 +199,3 @@ public class FirstSceneActivity extends AppCompatActivity {
         перейтиКСцене(новаяСцена);
     }
 }
-                                                                                                                                 
